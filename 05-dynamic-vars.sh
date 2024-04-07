9ea97c5924792d97457dda11bda741ac1edd0573
Azhar <azharshaikh33@gmail.com>
2024-04-07 16:23:02 +0400

    AutoPush

--- 05-dynamic-vars.sh
+++ 05-dynamic-vars.sh
@@ -2,5 +2,5 @@
 
 DATE=$(date +%F)
 
-echo -e "Welcome to training today date is \e[31m ${DATE} \e[0m"
-echo -e "Current user session in the system are : \e[31m $(who | wc -l) \e[0m"
+echo -e "Welcome to training today date is \e[32m ${DATE} \e[0m"
+echo -e "Current user session in the system are : \e[32m $(who | wc -l) \e[0m"
