389d3ea6b7a86e31b1d34bbd78a8ef1e996e5a7e
Azhar <azharshaikh33@gmail.com>
2024-04-08 14:24:47 +0400

    AutoPush

--- 09-functions.sh
+++ 09-functions.sh
@@ -10,3 +10,13 @@ sample
 sample
 sample
 sample
+
+stat () {
+    echo "Total number os session: $(who | wc -l)"
+    echo "Todat date is $(date +F%)"
+    echo " Load average of the system is $(uptime | awk -F : '{print $NF}' | awk -F , '{print $1}')"
+    echo "stat function completed"
+}
+
+echo calling stat function
+stat
