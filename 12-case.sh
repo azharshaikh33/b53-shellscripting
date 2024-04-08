05241836cf202fb3bd22128481ec74a939e8a1ca
Azhar <azharshaikh33@gmail.com>
2024-04-08 15:42:42 +0400

    AutoPush

--- 12-case.sh
+++ 12-case.sh
@@ -6,17 +6,21 @@ case $ACTION in
 
 start)
     echo "Starting the payment service"
+    exit 0
     ;;
 
 stop)
     echo "Stopping the paymet service"
+    exit 2
     ;;
 
 Restart)
     echo "Restarting the payment service"
+    exit 3
     ;;
 
 *)
     echo "Valid options are Start or Stop or Restart"
+    exit 4
     ;;
 esac
