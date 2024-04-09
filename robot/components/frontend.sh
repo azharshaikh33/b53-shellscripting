9a91f13f8fe145ba6ce400d9f131409145cf9f11
Azhar <azharshaikh33@gmail.com>
2024-04-09 14:21:12 +0400

    AutoPush

--- robot/components/frontend.sh
+++ robot/components/frontend.sh
@@ -4,7 +4,7 @@ set -e
 
 ID=$(id -u)
 if [ "$ID" -ne 0 ] ; then
-    echo -e "\e[32m You should execute this script as a root user or with a sudo as a prefix \e[0m"
+    echo -e "\e[31m You should execute this script as a root user or with a sudo as a prefix \e[0m"
     exit 1
 fi    
 
