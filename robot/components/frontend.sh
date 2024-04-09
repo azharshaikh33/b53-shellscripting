f03d57281455e48c341904fa14097bbc9fdc4e23
Azhar <azharshaikh33@gmail.com>
2024-04-09 14:51:22 +0400

    AutoPush

--- robot/components/frontend.sh
+++ robot/components/frontend.sh
@@ -9,11 +9,12 @@ if [ "$ID" -ne 0 ] ; then
 fi    
 
 echo -n "Installing Nginx : "
-dnf install qwasaser -y &>> /tmp/frontend.log
+dnf install nginx -y &>> /tmp/frontend.log
 if [ $? -eq 0 ] ; then
     echo -e "\e[32m Success \e[0m"
 else
     echo -e "\e[31m Failure \e[0m"
+    exit 2
 fi
 
 curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
