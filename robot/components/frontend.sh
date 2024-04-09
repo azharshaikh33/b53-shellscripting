53e95eb086224153d40bd183db8ca54a798eb299
Azhar <azharshaikh33@gmail.com>
2024-04-09 14:23:41 +0400

    AutoPush

--- robot/components/frontend.sh
+++ robot/components/frontend.sh
@@ -8,20 +8,20 @@ if [ "$ID" -ne 0 ] ; then
     exit 1
 fi    
 
-dnf install nginx -y 
+dnf install nginx -y &>> /tmp/frontend.log
 
 curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
 
 cd /usr/share/nginx/html
-rm -rf *
-unzip /tmp/frontend.zip
+rm -rf * &>> /tmp/frontend.log
+unzip /tmp/frontend.zip &>> /tmp/frontend.log
 mv frontend-main/* .
 mv static/* .
 rm -rf frontend-main README.md
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
 
-systemctl enable nginx
-systemctl restart nginx
+systemctl enable nginx &>> /tmp/frontend.log
+systemctl restart nginx &>> /tmp/frontend.log
 
 # curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
 
