d51f1e315348ca9ab9bb89e00a6a3162700caee5
Azhar <azharshaikh33@gmail.com>
2024-04-09 15:28:48 +0400

    AutoPush

--- robot/components/mongodb.sh
+++ robot/components/mongodb.sh
@@ -1,3 +1,32 @@
 #!/bin/bash
 
-echo "I am mongodb"
+# set -e
+
+COMPONENT=mongo
+LOGFILE="/tmp/$COMPONENT.log"
+
+ID=$(id -u)
+
+if [ "$ID" -ne 0 ] ; then
+    echo -e "\e[31m You should execute this script as a root user or with a sudo as a prefix \e[0m"
+    exit 1
+fi    
+
+stat() {
+    if [ $1 -eq 0 ] ; then
+        echo -e "\e[32m Success \e[0m"
+    else
+        echo -e "\e[31m Failure \e[0m"
+        exit 2
+    fi
+}
+
+echo -n "Configuring the $COMPONENT repo"
+curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/stans-robot-project/mongodb/main/mongo.repo
+stat $?
+
+echo -n "Installing $COMPONENT"
+yum install -y mongodb-org &>> $LOGFILE
+stat $?
+
+
