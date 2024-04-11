c9a40679a0ea8e75e957c432d72c9b6269fe6a12
Azhar <azharshaikh33@gmail.com>
2024-04-11 20:47:56 +0400

    AutoPush

--- robot/components/mysql.sh
+++ robot/components/mysql.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 
-echo "I am mysql"
+# set -e
+
+COMPONENT=mysql
+source components/common.sh
+
+echo -n "Configuring the $COMPONENT repo:"
+dnf module disable mysql -y 
+curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/stans-robot-project/mysql/main/mysql.repo
+stat $?
+
+echo -n "Installing the $COMPONENT:"
+dnf install mysql-community-server -y
+stat $?
+
+echo -n "STarting $COMPONENT:"
+systemctl enable mysqld 
+systemctl start mysqld
+stat $?
