a089e700b9f03899a95ebd45a518b41bd17e6989
Azhar <azharshaikh33@gmail.com>
2024-04-11 15:41:22 +0400

    AutoPush

--- robot/components/common.sh
+++ robot/components/common.sh
@@ -60,7 +60,7 @@ NPM_INSTALL() {
 CONFIG_SVC() {
 
     echo -n "Updating the systemd files with DB details:"
-    sed -i -e 's/MONGO_DNSNAME/mongodb.azharpro.internal/' /home/$APPUSER/$COMPONENT/systemd.service
+    sed -i -e 's/REDIS_ENDPOINT/redis.azharpro.internal/' -e 's/CATALOGUE_ENDPOINT/catalogue.azharpro.internal/' -e 's/MONGO_DNSNAME/mongodb.azharpro.internal/' -e 's/REDIS_ENDPOINT/redis.azharpro.internal/' -e 's/MONGO_ENDPOINT/mongodb.azharpro.internal/' /home/$APPUSER/$COMPONENT/systemd.service
     mv /home/$APPUSER/$COMPONENT/systemd.service /etc/systemd/system/$COMPONENT.service
     stat $?
 
