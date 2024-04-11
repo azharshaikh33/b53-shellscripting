ab217640190ac476fcb966f1cce19018e385325c
Azhar <azharshaikh33@gmail.com>
2024-04-11 21:37:18 +0400

    AutoPush

--- robot/components/mysql.sh
+++ robot/components/mysql.sh
@@ -23,6 +23,11 @@ echo -n "Grab $COMPONENT default password:"
 DEFAULT_ROOT_PWD=$(grep "temporary password" /var/log/mysqld.log | awk '{print $NF}')
 stat $?
 
-echo -n "Password reset of root user:"
-echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'RoboSHop@1';" | mysql --connect-expired-password -uroot -p${DEFAULT_ROOT_PWD} &>> $LOGFILE
-stat $?
+echo "show databases;" | -uroot -pRoboShop@1 &>> $LOGFILE
+if [ $? -ne 0 ] ; then
+
+    echo -n "Password reset of root user:"
+    echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'RoboSHop@1';" | mysql --connect-expired-password -uroot -p${DEFAULT_ROOT_PWD} &>> $LOGFILE
+    stat $?
+    
+fi
