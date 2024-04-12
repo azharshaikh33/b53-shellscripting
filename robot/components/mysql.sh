3cb940ce43bf9b0b97e2a00ed801d4dd501efb02
Azhar <azharshaikh33@gmail.com>
2024-04-12 07:39:48 +0400

    autopush

--- robot/components/mysql.sh
+++ robot/components/mysql.sh
@@ -20,7 +20,7 @@ systemctl start mysqld  &>> $LOGFILE
 stat $?
 
 echo -n "Grab $COMPONENT default password:"
-DEFAULT_ROOT_PWD=$(grep "temporary password" /var/log/mysqld.log | awk '{print $NF}')
+DEFAULT_ROOT_PWD=$(grep "temporary password" /var/log/mysqld.log | awk '{print $NF}') &>> $LOGFILE
 stat $?
 
 echo "show databases;" | mysql -uroot -pRoboShop@1 &>> $LOGFILE
