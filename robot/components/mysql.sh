ccee5022b777bfa9637319b8e95e98ec0ec479a7
Azhar <azharshaikh33@gmail.com>
2024-04-12 07:36:32 +0400

    autopush

--- robot/components/mysql.sh
+++ robot/components/mysql.sh
@@ -51,6 +51,7 @@ unzip -o /tmp/$COMPONENT.zip &>> $LOGFILE
 stat $?
 
 echo -n "Injecting the schema:"
-mysql -u root -pRoboShop@1 <shipping.sql &>> $LOGFILE
+cd $COMPONENT-main
+mysql -u root -pRoboShop@1 <shipping.sql
 stat $?
 
