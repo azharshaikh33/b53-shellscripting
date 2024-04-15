056e7a89058acbd03bd83c360751d5972bcc79bf
Azhar <azharshaikh33@gmail.com>
2024-04-15 08:17:34 +0400

    AutoPush

--- robot/create-ec2.sh
+++ robot/create-ec2.sh
@@ -15,4 +15,7 @@ AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-Cen
 SGID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=b53-allow-all-sg | jq '.SecurityGroups[].GroupId" | sed 's/"//g')
 
 echo -n "Launching the Instance with $AMI_ID as AMI :"
-aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro --security-group-ids ${SGID} --tag-specifications "ResourceType=instance,Tags=[{Key=name,Value=$COMPONENT}]" | jq
+aws ec2 run-instances --image-id $AMI_ID \
+                      --instance-type t2.micro \
+                      --security-group-ids ${SGID} \
+                      --tag-specifications "ResourceType=instance,Tags=[{Key=name,Value=$COMPONENT}]" | jq
