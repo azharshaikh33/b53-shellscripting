c7ca6d6ef72baefaf2c2e46f8364cee58aad489a
Azhar <azharshaikh33@gmail.com>
2024-04-15 07:01:16 +0400

    AutoPush

--- robot/create-ec2.sh
+++ robot/create-ec2.sh
@@ -2,4 +2,4 @@
 
 #This is script to launch EC2 servers and create the associated Route53 Record.
 
-AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-Centos-8" | jq '.Images[].ImageId')
+AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-Centos-8" | jq '.Images[].ImageId') | sed -e 's/"//g'
