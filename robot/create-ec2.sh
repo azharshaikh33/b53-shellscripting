d3370acad925be087169f9fe9dc68f309db5b986
Azhar <azharshaikh33@gmail.com>
2024-04-15 08:11:45 +0400

    AutoPush

--- robot/create-ec2.sh
+++ robot/create-ec2.sh
@@ -20,4 +20,4 @@ echo -n "Launching the Instance with $AMI_ID as AMI:"
 aws ec2 run-instances --image-id $AMI_ID \
                     --instance-type t2.micro \
                     --security-group-ids $SGID \
-                    --tag-specifications "ResourceType=instance,Tags=[{Key=name,Value=$COMPONENT}]" | jq
+                    --tag-specifications "ResourceType=instance,Tags=[{Key=name,Value=$COMPONENT}]"
