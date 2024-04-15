a45c2a2dcdfbd0d445eecb330b2329f8d6e07393
Azhar <azharshaikh33@gmail.com>
2024-04-15 23:01:35 +0400

    AutoPush

--- robot/create-ec2.sh
+++ robot/create-ec2.sh
@@ -19,4 +19,5 @@ echo -n "Launching the Instance with $AMI_ID as AMI :"
 aws ec2 run-instances --image-id ${AMI_ID} \
                       --instance-type t2.micro \
                       --security-group-ids ${SGID} \
+                      --instance-market-options "MarketType=spot, SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=Stop}" \
                       --tag-specifications "ResourceType=instance,Tags=[{Key=name,Value=$COMPONENT}]" | jq
