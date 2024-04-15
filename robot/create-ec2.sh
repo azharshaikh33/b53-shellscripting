4caf5c90af4789f2460d9fc93ab3c2aee54df837
Azhar <azharshaikh33@gmail.com>
2024-04-15 23:29:57 +0400

    AutoPush

--- robot/create-ec2.sh
+++ robot/create-ec2.sh
@@ -10,14 +10,20 @@ if [ -z "$1" ]; then
 
 fi
 
+HOSTEDZONEID="Z093296193U6R55S5C7L"
 COMPONENT=$1
 
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-Centos-8" | jq '.Images[].ImageId' | sed -e 's/"//g')
 SGID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=b53-allow-all-sg | jq '.SecurityGroups[].GroupId' | sed -e 's/"//g')
 
+echo Ami ID is $AMI_ID
+
 echo -n "Launching the Instance with $AMI_ID as AMI :"
-aws ec2 run-instances --image-id ${AMI_ID} \
+IPADDRESS=$(aws ec2 run-instances --image-id ${AMI_ID} \
                       --instance-type t2.micro \
                       --security-group-ids ${SGID} \
                       --instance-market-options "MarketType=spot, SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=Stop}" \
-                      --tag-specifications "ResourceType=instance,Tags=[{Key=name,Value=$COMPONENT}]" | jq
+                      --tag-specifications "ResourceType=instance,Tags=[{Key=name,Value=$COMPONENT}]" | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
+
+sed -e "s/COMPONENT/${COMPONENT}/" -e "s/IPADDRESS/${IPADDRESS}/" route53.json > /tmp/r53.json
+aws route53 change-resource-record-sets --hosted-zone-id $HOSTEDZONEID --change-batch file:///tmp/r53.json
